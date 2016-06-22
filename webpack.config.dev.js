a49e3ec3eaef9c9e1e2e006c67a5cc35c0610d7e
Diego Arcega <diegoarcega@gmail.com>
2016-06-22 17:36:30 -0300

    dist

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,23 +19,15 @@ var CONFIG = {
     },
     module: {
         loaders: [
-            { test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel', query:{ presets:['es2015','react'] } },
+            { test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel' },
             { test: /\.(css|scss)$/, loaders: ['style', 'css?modules&importLoaders=1&localIdentName=[local]_[hash:base64:5]', 'sass'] },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
             { test: /\.jpg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
             { test: /\.png(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-            { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
         ]
     },
     plugins: [
       new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
-        }
-      }),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
