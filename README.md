1bb7f890e28539fea8b2ea73eaad6342ee266031
Diego Arcega <diegoarcega@gmail.com>
2016-06-23 15:17:03 -0300

    dist v

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ v4.4.5
 npm
 v3.9.6
 
-> Note: Running a Babel 6.x project using npm 2.x can cause performance problems because of the way npm 2.x installs dependencies. This problem can be eliminated by either switching to npm 3.x or running npm 2.x with the dedupe flag. To check what version of npm you have r
+> Note: Running a Babel 6.x project using npm 2.x can cause performance problems because of the way npm 2.x installs dependencies. This problem can be eliminated by either switching to npm 3.x or running npm 2.x with the dedupe flag.
+
+---
 
 > styles are not being addressed at this moment, so I am just importing them from a cdn
--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -6,7 +6,7 @@ var SRC_DIR = path.resolve(__dirname, 'src');
 var CONFIG = {
     entry: SRC_DIR + '/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/assets'),
         filename: 'bundle.js',
         publicPath: '/assets/'
     },
@@ -19,13 +19,10 @@ var CONFIG = {
     },
     module: {
           loaders: [
-              { test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel', query:{ presets:['es2015','react'] } },
+              { test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel-loader' },
               { test: /\.(css|scss)$/, loaders: ['style', 'css?modules&importLoaders=1&localIdentName=[local]_[hash:base64:5]', 'sass'] },
-              { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
               { test: /\.jpg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
               { test: /\.png(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-              { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-              { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
               { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
           ]
       },
