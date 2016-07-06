dc8a09578fbde44b78553ea2a01ecfaa3f5ad7de
Diego Arcega <diegoarcega@gmail.com>
2016-07-06 14:51:45 -0300

    server

--- server/server.js
+++ server/server.js
@@ -1,46 +1,46 @@
-var cool = require('cool-ascii-faces');
+
 var express = require('express');
 var path = require('path');
+//
+// var webpackDevMiddleware require('webpack-dev-middleware')
+// var webpackHotMiddleware require('webpack-hot-middleware')
+// var webpackDevConfigs require('./webpack.dev')
+// var webpack require('webpack')
+//
+// var compiler = webpack(webpackDevConfigs)
+var app = express();
 
-var webpackDevMiddleware require('webpack-dev-middleware')
-var webpackHotMiddleware require('webpack-hot-middleware')
-var webpackDevConfigs require('./webpack.dev')
-var webpack require('webpack')
-
-var compiler = webpack(webpackDevConfigs)
-var app = express()
-
-var DIST_DIR = path.resolve(__dirname + '/../dist')
-var SRC_DIR = path.resolve(__dirname + '/../src/')
+var DIST_DIR = path.resolve(__dirname + '/../dist');
+// var SRC_DIR = path.resolve(__dirname + '/../src/')
 
 /* Development */
 /* It doesn't use the /dist folder's files, all the files sits in memory */
-if(process.env.NODE_ENV === 'development'){
-  app.use(webpackDevMiddleware(compiler,{
-     stats: {colors: true},
-     hot: true,
-     historyApiFallback: true,
-     publicPath: '/assets/'
-  }))
-  app.use(webpackHotMiddleware(compiler,{
-    log: console.log,
-    path: '/__webpack_hmr',
-  }))
-  app.get('*', function(req, res) {
-    res.sendFile(SRC_DIR + '/index.html')
-  })
-}
+// if(process.env.NODE_ENV === 'development'){
+//   app.use(webpackDevMiddleware(compiler,{
+//      stats: {colors: true},
+//      hot: true,
+//      historyApiFallback: true,
+//      publicPath: '/assets/'
+//   }))
+//   app.use(webpackHotMiddleware(compiler,{
+//     log: console.log,
+//     path: '/__webpack_hmr',
+//   }))
+//   app.get('*', function(req, res) {
+//     res.sendFile(SRC_DIR + '/index.html')
+//   })
+// }
 
 /* Production */
-if(process.env.NODE_ENV === 'production'){
-  app.use('/assets/', express.static(DIST_DIR + '/assets'))
+// if(process.env.NODE_ENV === 'production'){
+  app.use('/assets/', express.static(DIST_DIR + '/assets'));
   app.get('*', function(req, res) {
-    res.sendFile(DIST_DIR + '/index.html')
-  })
-}
+    res.sendFile(DIST_DIR + '/index.html');
+  });
+// }
 
 /* Server */
 app.listen(process.env.PORT || 5000, function() {
-  console.log('Listening port ' + process.env.PORT || 5000)
-  console.log('You are in ' + process.env.NODE_ENV + ' environment')
-})
+  console.log('Listening port ' + process.env.PORT || 5000);
+  console.log('You are in ' + process.env.NODE_ENV + ' environment');
+});
